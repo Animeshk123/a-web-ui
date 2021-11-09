1b8abb6a8d6b221d06956aa6612526ae1697893c
Animesh kumbhakar <75767221+Animeshk123@users.noreply.github.com>
2021-11-09 15:21:20 +0530

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || "7000";
 app.use(express.static("public"));
-app.listen("5000",() => {
+app.listen(PORT,() => {
 	console.log("server started on port 5000")
 	});
